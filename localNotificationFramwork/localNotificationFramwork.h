4dda55e2bb61a0dd15052c67d01fc4dcba5a144d
Saloni231 <ghagsaloni231@gmail.com>
2022-09-26 00:26:40 -0700

    Initial Commit

---
+++ localNotificationFramwork/localNotificationFramwork.h
@@ -0,0 +1,18 @@
+//
+//  localNotificationFramwork.h
+//  localNotificationFramwork
+//
+//  Created by Capgemini-DA078 on 25/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for localNotificationFramwork.
+FOUNDATION_EXPORT double localNotificationFramworkVersionNumber;
+
+//! Project version string for localNotificationFramwork.
+FOUNDATION_EXPORT const unsigned char localNotificationFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <localNotificationFramwork/PublicHeader.h>
+
+
---
+++ localNotificationFramwork/orderPlacedNotification.swift
@@ -0,0 +1,37 @@
+//
+//  orderPlacedNotification.swift
+//  localNotificationFramwork
+//
+//  Created by Capgemini-DA078 on 25/09/22.
+//
+
+import Foundation
+import UserNotifications
+
+public class OrderPlacedNotifiction {
+    
+    public init() {
+        
+    }
+    
+    //MARK: Local Notification Function
+    public func orderPlaced() {
+        
+        let notifContent = UNMutableNotificationContent()
+        
+        notifContent.title = "Shopping App"
+        notifContent.body = "Order Placed Successfully.\n Thanks for using app."
+        notifContent.badge = NSNumber(value: 1)
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let notifRequest = UNNotificationRequest(identifier: "ShoppingApp", content: notifContent, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(notifRequest) {
+            (error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
